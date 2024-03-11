41b3077953a40298d79b22b90de48acc0fa00b85
samuelkanyingi <samuelkanyingi2016@gmail.com>
2024-03-11 05:26:31 -0700

    Add a custom HTTP header

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,26 +1,30 @@
-#Add a custom HTTP header with Puppet
-{ 'update system':
-  command => '/usr/bin/apt-get update',
+# Setup New Ubuntu server with nginx
+# and add a custom HTTP header
+
+exec { 'update system':
+        command => '/usr/bin/apt-get update',
 }
 
 package { 'nginx':
-  ensure  => 'installed',
-  require => Exec['update system'],
+	ensure => 'installed',
+	require => Exec['update system']
 }
 
-file { '/var/www/html/index.html':
-  content => 'Hello World!',
+file {'/var/www/html/index.html':
+	content => 'Hello World!'
 }
 
-file { '/etc/nginx/sites-available/default':
-  ensure  => 'file',
-  content => template('my_module/nginx_config.erb'),
-  require => Package['nginx'],
-  notify  => Service['nginx'],
+exec {'redirect_me':
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://th3-gr00t.tk/ permanent;" /etc/nginx/sites-available/default',
+	provider => 'shell'
 }
 
-service { 'nginx':
-  ensure  => 'running',
-  require => Package['nginx'],
+exec {'HTTP header':
+	command => 'sed -i "25i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
+	provider => 'shell'
 }
 
+service {'nginx':
+	ensure => running,
+	require => Package['nginx']
+}
