7ec25fdbdf6fc3ea681668597a03eedfe545b572
samuelkanyingi <samuelkanyingi2016@gmail.com>
2024-03-11 05:39:43 -0700

    Add a custom HTTP header

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,21 +1,20 @@
-# Setup New Ubuntu server with nginx
-# and add a custom HTTP header
-
-exec { 'update system':
+# Automate the task of creating a custom HTTP header response
+# with Puppet
+exec { 'update':
         command => '/usr/bin/apt-get update',
 }
 
 package { 'nginx':
 	ensure => 'installed',
-	require => Exec['update system']
+	require => Exec['update']
 }
 
 file {'/var/www/html/index.html':
-	content => 'Hello World!'
+	content => 'Hello there!'
 }
 
 exec {'redirect_me':
-	command => 'sed -i "24i\	rewrite ^/redirect_me https://th3-gr00t.tk/ permanent;" /etc/nginx/sites-available/default',
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://samservices.tech/  permanent;" /etc/nginx/sites-available/default',
 	provider => 'shell'
 }
 
