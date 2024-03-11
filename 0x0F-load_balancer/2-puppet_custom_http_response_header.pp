76e78e645a2eaa74f12d0c0f92e49330b5667531
samuelkanyingi <samuelkanyingi2016@gmail.com>
2024-03-11 13:29:11 -0700

    Add a custom HTTP header with Puppet+

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -14,12 +14,12 @@ file {'/var/www/html/index.html':
 }
 
 exec {'redirect_me':
-	command => 'sed -i "i\	rewrite ^/redirect_me https://samservices.tech/  permanent;" /etc/nginx/sites-available/default',
+	command => 'sed -i "24i\ rewrite ^/redirect_me https://samservices.tech/  permanent;" /etc/nginx/sites-available/default',
 	provider => 'shell'
 }
 
 exec {'HTTP header':
-	command => 'sed -i "25i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
+	command => 'sed -i "25i\ add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
 	provider => 'shell'
 }
 
