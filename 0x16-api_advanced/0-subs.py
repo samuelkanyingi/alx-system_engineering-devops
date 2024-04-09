d026ab2f2c3c05399695e097e6f599e4972d9110
samuelkanyingi <samuelkanyingi2016@gmail.com>
2024-04-09 04:45:36 -0700

    How many subs?

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+''' Write a function to find How many subs '''
+import requests
+
+
+def number_of_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Custom User Agent'}
+
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()  # Raise an error for bad response status
+
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+
+    except requests.HTTPError as e:
+        if e.response.status_code == 404:
+            print("Subreddit not found.")
+        else:
+            print("An error occurred:", e)
+
+    except Exception as e:
+        print("An error occurred:", e)
+
+    return 0
