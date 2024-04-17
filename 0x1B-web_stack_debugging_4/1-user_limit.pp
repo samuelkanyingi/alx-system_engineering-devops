f25e810a58cfde754118e5f412dd2108bda1c14f
samuelkanyingi <samuelkanyingi2016@gmail.com>
2024-04-17 07:36:52 +0000

    user-limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,14 @@
 #increase file descriptor limits for the holberton user
 exec { 'increase-holberton-limits':
-  command => 'sed -i "s/holberton hard nofile 5/holberton hard nofile 5000/" /etc/security/limits.conf && sed -i "s/holberton soft nofile 4/holberton soft nofile 4000/" /etc/security/limits.conf && /sbin/sysctl -p',
+  command => 'sed -i "s/holberton hard nofile 5/holberton hard nofile 5000/" /etc/security/limits.conf',
   path    => '/usr/bin:/usr/sbin:/bin',
+}
+
+exec { 'increase-limits':
+  command => 'sed -i "s/holberton soft nofile 4/holberton soft nofile 4000/" /etc/security/limits.conf',
+  path    => '/usr/bin:/usr/sbin:/bin',
+}
+exec { 'refresh':
+  command => '/sbin/sysctl -p',
+  path    => '/sbin',
 }
